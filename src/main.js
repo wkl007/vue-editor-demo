ed28eb92e4eb19f914baf0b9574be2362917357d
wkl--007 <499657357@qq.com>
2020-02-09 16:18:17 +0800

    chore: 项目框架搭建

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import App from '@/App.vue'
+import '@/configs/registerServiceWorker'
+import router from '@/router'
 
 Vue.config.productionTip = false
 
